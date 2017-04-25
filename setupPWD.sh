7aa94f94a60b018424434d02e3d40573c9f958b2
Steve Griffith <stgriffi@microsoft.com>
2017-04-25 10:49:43 -0400

    Updated shell script

--- setupPWD.sh
+++ setupPWD.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-
+echo "Worked" >> ~/testfile
 # exit on any error
-set -e
+#set -e
 
-echo "Welcome to setupPWD.sh"
+#echo "Welcome to setupPWD.sh"
 
 
 # install needed bits in a loop because a lot of installs happen
 # on VM init, so won't be able to grab the dpkg lock immediately
-until apt-get -y update && apt-get -y install golang git docker.io
-do
-  echo "Trying again"
-  sleep 2
-done
+#until apt-get -y update && apt-get -y install golang git docker.io
+#do
+#  echo "Trying again"
+#  sleep 2
+#done
 
-cd /usr/share/go/src
-mkdir github.com
-cd github.com
-mkdir franela
-cd franela
-git clone https://github.com/franela/play-with-docker.git
+#cd /usr/share/go/src
+#mkdir github.com
+#cd github.com
+#mkdir franela
+#cd franela
+#git clone https://github.com/franela/play-with-docker.git
 
-docker swarm init
-sudo modprobe xt_ipvs
-go get -v -d -t ./...
-docker-compose up
+#docker swarm init
+#sudo modprobe xt_ipvs
+#go get -v -d -t ./...
+#docker-compose up
 
 
-echo "Done!"
+#echo "Done!"
